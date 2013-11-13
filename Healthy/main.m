66679fae94c979e194e8bacf7e7b4e14ff978aff
miclle <miclle.zheng@gmail.com>
2013-11-13 11:23:25 +0800

    Initial Commit

---
+++ Healthy/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Healthy
+//
+//  Created by Miclle Zheng on 13-11-13.
+//  Copyright (c) 2013年 JKYD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
